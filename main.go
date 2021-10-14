5e0a06a86f20cfd0b55a7a05b381f139f26ff052
Maxizx <83468300+Maxizx@users.noreply.github.com>
2021-10-14 17:39:36 -0300

    Update main.go
    ejer8

--- main.go
+++ main.go
@@ -1,15 +1,91 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	var num int
-	fmt.Print("elegir un numero del 1 al 5\n")
-	fmt.Scan(&num)
+	for {
+		ok := ejer8()
+		if !ok {
+			break
+		}
+	}
+
+}
+
+func ejer1() {
+
+	var pal string
+	fmt.Println("Ejercicio 1.")
+	//xd := bufio.NewReader(os.Stdin)
+	fmt.Println("Escribe una frase.")
+	fmt.Scan(&pal)
+	//frase, _ := xd.ReadString('\n')
+	fmt.Println("Tu frase fue:", pal)
+
+}
+
+func ejer2() {
+
+	fmt.Println("Ejercicio 2")
+	fmt.Println("Escribe un numero.")
+	var w int
+	fmt.Scan(&w)
+	fmt.Printf("Tu numero es: %d\n ", w)
+}
+
+func ejer3() {
+
+	fmt.Println("ejercicio 3")
+	fmt.Println("Escribe tu primer num")
+	var x, y int
+	fmt.Scan(&x)
+	fmt.Println("Escribe tu segundo num.")
+	fmt.Scan(&y)
+	fmt.Printf("total %d\n", x+y)
+}
+
+func ejer4() {
+
+	fmt.Println("ejercicio 4")
+	fmt.Println("Escribe tu nombre")
+	var (
+		nombre, apellido string
+		edad             int
+	)
+
+	fmt.Scan(&nombre)
+	fmt.Println("Escribe tu apellido")
+	fmt.Scan(&apellido)
+	fmt.Println("Escribe tu edad")
+	fmt.Scan(&edad)
+
+	fmt.Printf("Te llamas %s", nombre)
+	fmt.Printf(", tu apellido es %s", apellido)
+	fmt.Printf(" y tenes %d\n", edad)
+}
+
+func ejer5() {
+
+	var a, b, c, d, e int
+	fmt.Println("Escribe tu primer num")
+	fmt.Scan(&a)
+	fmt.Println("Escribe tu segundo num.")
+	fmt.Scan(&b)
+	fmt.Println("Escribe tu 3er num.")
+	fmt.Scan(&c)
+	fmt.Println("Escribe tu 4to num.")
+	fmt.Scan(&d)
+	fmt.Println("Escribe tu 5to num.")
+	fmt.Scan(&e)
+	fmt.Println(a, "+", b, "+", c, "+", d, "+", e, "=", a+b+c+d+e)
+
+	fmt.Println("Tareas realizadas con exito")
+
+}
+
+func ejer7(num int) {
 
 	switch num {
 
@@ -34,88 +110,22 @@ func main() {
 		//break
 
 	default:
-		fmt.Println("¿sos idiota? del 1 al 5 te dije")
+		fmt.Println("¿Sos idiota? del 1 al 5 te dije")
 	}
 
 }
 
-func ejer1() {
+func ejer8() bool {
+	fmt.Print("elegir un numero del 1 al 5, si quiere salir ingrese 0\n")
+	var num int
+	fmt.Scan(&num)
 
-	fmt.Println("Ejercicio 1.")
-	xd := bufio.NewReader(os.Stdin)
-	fmt.Println("Escribe una frase.")
-	//var frase stri
-	//frase = "hola como estas"
-	//fmt.Scanln(&frase
-	//frasea := bufio.NewScanner((os.Stdin))
-	//fmt.Printf("Tu frase fue: %s", frase)
-	frase, _ := xd.ReadString('\n')
-	fmt.Println("Tu frase fue:", frase)
-	//str := bufio.NewScanner(os.Stdin)
-	//fmt.Printf("Tus frase fue: %s", str.Text())
-}
+	fmt.Println(num)
+	if num != 0 && (num >= 1 && num <= 5) {
+		ejer7(num)
+		return true
+	} else {
+		return false
+	}
 
-///////////////////////////////////////////////////////////////////////////////
-func ejer2() {
-
-	//fmt.Println("----------------------------------------------------")
-	fmt.Println("Ejercicio 2")
-	fmt.Println("Escribe un numero.")
-	var w int
-	fmt.Scan(&w)
-	fmt.Printf("Tu numero es: %d\n ", w)
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////
-func ejer3() {
-
-	//fmt.Println("----------------------------------------------------")
-	fmt.Println("ejercicio 3")
-	fmt.Println("Escribe tu primer num")
-	var x, y int
-	fmt.Scan(&x)
-	fmt.Println("Escribe tu segundo num.")
-	fmt.Scan(&y)
-	fmt.Printf("total %d\n", x+y)
-}
-
-func ejer4() {
-
-	//fmt.Println("----------------------------------------------------")
-	fmt.Println("ejercicio 4")
-	fmt.Println("Escribe tu nombre")
-	var (
-		nombre, apellido string
-		edad             int
-	)
-
-	fmt.Scan(&nombre)
-	fmt.Println("Escribe tu apellido")
-	fmt.Scan(&apellido)
-	fmt.Println("Escribe tu edad")
-	fmt.Scan(&edad)
-
-	fmt.Printf("Te llamas %s", nombre)
-	fmt.Printf(", tu apellido es %s", apellido)
-	fmt.Printf(" y tenes %d\n", edad)
-}
-
-func ejer5() {
-
-	//fmt.Println("----------------------------------------------------")
-	var a, b, c, d, e int
-	fmt.Println("Escribe tu primer num")
-	fmt.Scan(&a)
-	fmt.Println("Escribe tu segundo num.")
-	fmt.Scan(&b)
-	fmt.Println("Escribe tu 3er num.")
-	fmt.Scan(&c)
-	fmt.Println("Escribe tu 4to num.")
-	fmt.Scan(&d)
-	fmt.Println("Escribe tu 5to num.")
-	fmt.Scan(&e)
-	fmt.Println(a, "+", b, "+", c, "+", d, "+", e, "=", a+b+c+d+e)
-
-	fmt.Println("Tareas realizadas con exito")
-	//fmt.Printf("%d %s", a+b+c+d+e)
 }
